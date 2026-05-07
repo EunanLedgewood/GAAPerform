06a5080a4c3b02ad38f82aee62ea3813a54c3292
Eunan <eledgewood2001@gmail.com>
2026-05-08 00:23:30 +0100

    Registering services and viewmodels in MauiProgram (dependency injection)

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,25 +1,37 @@
 ﻿using Microsoft.Extensions.Logging;
+using GAAPerform.Services;
+using GAAPerform.ViewModels;
 
-namespace GAAPerform
+namespace GAAPerform;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+        var builder = MauiApp.CreateBuilder();
+
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        // Services
+        builder.Services.AddSingleton<DatabaseService>();
+        builder.Services.AddSingleton<TrainingPlanService>();
+
+        // ViewModels
+        builder.Services.AddTransient<WeekViewModel>();
+        builder.Services.AddTransient<ReadinessViewModel>();
+        builder.Services.AddTransient<LogViewModel>();
+        builder.Services.AddTransient<ProfileViewModel>();
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
-        }
+        return builder.Build();
     }
 }
